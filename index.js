b1e2d3b907ec8f439ad6d8fc53feffa7887908fe
Ali Raza <ali.4aug24webgpt@gmail.com>
2025-11-15 11:01:23 -0800

    2nd commit with dotenv added

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
 const app = express();
 const port = 3000;
+import dotenv from 'dotenv';
+dotenv.config();
+// iQDJrlrjduSZ4lTV
+// mongodb+srv://ali4aug24webgpt:iQDJrlrjduSZ4lTV@cluster0.1cvcyyq.mongodb.net/?appName=Cluster0
+const connectionDb = async () => {
+    try {
+        console.log("Connecting to DB...");
+
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log("Database connected successfully");
+        
+    } catch (err) {
+        console.log("Database connection failed:", err);
+    }
+};
+
+connectionDb();
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
