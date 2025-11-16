941c85a2538cc11fb4779338121cbf1384fc840c
Ali Raza <ali.4aug24webgpt@gmail.com>
2025-11-16 08:39:57 -0800

    chnages

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const connectionDb = async () => {
 connectionDb();
 
 app.get('/', (req, res) => {
-  res.json('Hello World! Here is Ali Raza Afzal First backend project deployement...');
+  res.json('Hello World! Here is Ali Raza Afzal first node app connected with MongoDB deployeed');
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
